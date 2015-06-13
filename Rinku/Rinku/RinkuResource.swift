09fb383f045bebf7b80d77e02d2f400a8d10443d
Rui Peres <rui.peres@just-eat.com>
2015-06-12 18:06:22 -0700

    Updated RinkuResource

--- Rinku/Rinku/RinkuResource.swift
+++ Rinku/Rinku/RinkuResource.swift
@@ -2,16 +2,15 @@
 //  RinkuResource.swift
 //  Rinku
 //
-//  Created by Rui Peres on 12/06/2014.
-//  Edit by Alexander Li on 12/23/2014.
-//  Copyright (c) 2014 Aphely. All rights reserved.
+//  Created by Rui Peres on 12/06/2015.
+//  Copyright (c) 2015 Rinku. All rights reserved.
 //
 
 import Foundation
 
 typealias Header = [String : String]
 
-public struct RinkuResource : CustomStringConvertible {
+public struct RinkuResource : Equatable, CustomStringConvertible {
     
     let path : String
     let method : Method
@@ -21,4 +20,18 @@ public struct RinkuResource : CustomStringConvertible {
     public var description : String {
         return "Path:\(path)\nMethod:\(method.rawValue)\n"
     }
+}
+
+public func == (lhs: RinkuResource, rhs: RinkuResource) -> Bool {
+    
+    var equalBody = false
+    
+    switch (lhs.body, rhs.body) {
+        case (nil,nil): equalBody = true
+        case (nil,_?) : equalBody = false
+        case (_?,nil) : equalBody = false
+        case (let l?,let r?) : equalBody = l.isEqualToData(r)
+    }
+    
+    return (lhs.path == rhs.path && lhs.method == rhs.method && equalBody)
 }
