4569bd8634ac1e3d98ed4e5a3eacc609a88a6cf9
Rui Peres <rui.peres@just-eat.com>
2015-06-12 18:06:46 -0700

    Initial work for the RinkyNetworkTask

---
+++ Rinku/Rinku/RinkuNetworkTask.swift
@@ -0,0 +1,15 @@
+//
+//  RinkuNetworkTask.swift
+//  Rinku
+//
+//  Created by Rui Peres on 12/06/2015.
+//  Copyright (c) 2015 Rinku. All rights reserved.
+//
+
+import Foundation
+
+struct RinkuNetworkTask {
+    
+    
+    
+}
