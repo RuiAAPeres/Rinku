fd5b01466a74c3857205461150bd394b9c6a7b96
Rui Peres <rui.peres@just-eat.com>
2015-06-12 18:27:54 -0700

    Further work on the Network Task

--- Rinku/Rinku/RinkuNetworkTask.swift
+++ Rinku/Rinku/RinkuNetworkTask.swift
@@ -8,8 +8,37 @@
 
 import Foundation
 
+public typealias CompletionHandler = NSData -> ()
+public typealias ProgressHandler = Double -> ()
+public typealias FailureHandler = (statusCode: Int, error: NSError?, data: NSData?) -> ()
+
+
 struct RinkuNetworkTask {
     
+    let request : NSURLRequest
+    var data : NSData
     
     
+    private var progressHandlers : [ProgressHandler] = []
+    private var failureHandlers : [FailureHandler] = []
+    private var completionHandlers : [CompletionHandler] = []
+    
+    mutating func addProgressHandler(progressHandler : ProgressHandler) -> () {
+        self.progressHandlers += [progressHandler]
+    }
+    
+    mutating func addCompletionHandler(completionHandler : CompletionHandler) -> () {
+        self.completionHandlers += [completionHandler]
+    }
+    
+    mutating func addFailureHandler(failureHandler : FailureHandler) -> () {
+        self.failureHandlers += [failureHandler]
+    }
+    
+    
+    func applyCompletionHandler() -> () {
+        self.completionHandlers.map { handler in
+            handler(data)
+        }
+    }
 }
