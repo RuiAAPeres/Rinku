4d0045da1f8848caa1d9f5f2beb83fe6764954da
Rui Peres <sauron.is.alive@gmail.com>
2015-06-23 21:21:32 +0100

    Added tests

---
+++ RinkuTests/RinkuNetworkingTaskTests.swift
@@ -0,0 +1,109 @@
+//
+//  RinkuNetworkingTaskTests.swift
+//  Rinku
+//
+//  Created by Rui Peres on 23/06/2015.
+//  Copyright © 2015 Rinku. All rights reserved.
+//
+
+import XCTest
+@testable import Rinku
+
+class RinkuNetworkingTaskTests: XCTestCase {
+    
+    func testEqualityShouldSucceedForSameURL() {
+        
+        let task1 = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        let task2 = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        
+        XCTAssertEqual(task1, task2)
+    }
+    
+    func testEqualityShouldFailForDifferentURL() {
+        
+        let task1 = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        let task2 = RinkuNetworkTask(url: NSURL(string: "www.yahoo.com")!)
+        
+        XCTAssertNotEqual(task1, task2)
+    }
+    
+    func testHashabilityOnURL() {
+        
+        let task1 = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        let task2 = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        let task3 = RinkuNetworkTask(url: NSURL(string: "www.yahoo.com")!)
+        
+        var uniqueTasks : Set<RinkuNetworkTask> = Set(arrayLiteral: task1,task2)
+        XCTAssert(uniqueTasks.count == 1)
+        
+        
+        uniqueTasks.insert(task3)
+        XCTAssert(uniqueTasks.count == 2)
+    }
+    
+    func testAddHandlers() {
+        var task = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        
+        let completionHandler : CompletionHandler = { (status, data , error) in }
+        let progressHandler : ProgressHandler = { (progress) in }
+
+        task.addHandlers(completionHandler, progress: progressHandler)
+        task.addHandlers(completionHandler, progress: progressHandler)
+        task.addHandlers(completionHandler, progress: progressHandler)
+        
+        XCTAssert(task.completionHandlers.count == 3)
+        XCTAssert(task.progressHandlers.count == 3)
+
+    }
+    
+    func testCompletionHandlers() {
+        var numberOfCompletions = 0
+        let expectation = expectationWithDescription("Completion Expectation")
+        
+        var task = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        
+        let completionHandler : CompletionHandler = { (status, data, error) -> () in
+            
+            XCTAssert(status == 200)
+            XCTAssert(error == nil)
+            XCTAssert(data.length == 0)
+            
+            if ((++numberOfCompletions) == 3) {
+                expectation.fulfill()
+            }
+        }
+        
+        task.addHandlers(completionHandler,progress: nil)
+        task.addHandlers(completionHandler,progress: nil)
+        task.addHandlers(completionHandler,progress:nil)
+        
+        task.applyCompletionHandler(200, error: nil)
+
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
+    
+    func testProgressHandlers() {
+        var numberOfCompletions = 0
+        let expectation = expectationWithDescription("Completion Expectation")
+        
+        var task = RinkuNetworkTask(url: NSURL(string: "www.google.com")!)
+        
+        let completionHandler : CompletionHandler = { (status, data , error) in }
+        let progressHandler : ProgressHandler = { (progress) in
+            
+            if ((++numberOfCompletions) == 51 * task.progressHandlers.count) {
+                expectation.fulfill()
+            }
+        }
+        
+        task.addHandlers(completionHandler, progress: progressHandler)
+        task.addHandlers(completionHandler, progress: progressHandler)
+        task.addHandlers(completionHandler, progress: progressHandler)
+        
+        for i in 0...100 where i % 2 == 0 {
+            task.applyProgressHandlers(Double(i))
+        }
+        
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
+}
